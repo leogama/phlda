56d9d8991213dc954fef34ced8973156cf5dc75a
Leonardo Gama <leonardo.reis.gama@gmail.com>
2018-11-22 18:59:28 -0200

    minor

--- lib/R/utils.R
+++ lib/R/utils.R
@@ -1,32 +1,3 @@
-setup <- memoise::memoise(function(packages) {
-    # find library's root directory
-    root_dir <- rprojroot::is_git_root$find_file()
-    if (getwd() != root_dir) setwd(root_dir)
-    
-    # wait server setup to finish
-    finished_setup <- 'tail -1 ~/.nb.setup.log | grep -q "Done initial azure notebooks environment setup"'
-    if (file.exists('~/.nb.setup.log')) {
-        cat('Waiting server to finish setup..')
-        retry(times=100L, delay=5L, {
-            cat('.')
-            stopifnot(!system(finished_setup))
-        })
-    }
-    
-    # load packages used here
-    .libPaths(c('env/lib/R', .libPaths()))
-    suppressPackageStartupMessages({
-        library(magrittr)
-        library(simpleCache)
-        setCacheDir('cache')
-        
-        if (!missing(packages)) lapply(packages, require, character=TRUE)
-    })
-    
-    # load and return global parameters
-    yaml::yaml.load_file('config.yml')
-})
-
 load_data <- function(gse_id) {
     simpleCache(sprintf('processed.%s', gse_id), {
         gse_id %>%
@@ -50,8 +21,37 @@ retry <- function(expr, times=5L, delay=0L) {
         n <<- n + 1L
         if (n < times)
             tryCatch(expr, error=wrapper)
-        else 
+        else
             expr
     }
     suppressWarnings(wrapper())
 }
+
+setup <- memoise::memoise(function(packages) {
+    # find library's root directory
+    root_dir <- rprojroot::is_git_root$find_file()
+    if (getwd() != root_dir) setwd(root_dir)
+
+    # wait server setup to finish
+    finished_setup <- 'tail -1 ~/.nb.setup.log | grep -q "Done initial azure notebooks environment setup"'
+    if (file.exists('~/.nb.setup.log')) {
+        cat('Waiting server to finish setup..')
+        retry(times=100L, delay=5L, {
+            cat('.')
+            stopifnot(!system(finished_setup))
+        })
+    }
+
+    # load packages used here
+    .libPaths(c('env/lib/R', .libPaths()))
+    suppressPackageStartupMessages({
+        library(magrittr)
+        library(simpleCache)
+        setCacheDir('cache')
+ 
+        if (!missing(packages)) lapply(packages, require, character=TRUE)
+    })
+
+    # load and return global parameters
+    yaml::yaml.load_file('config.yml')
+})
